4aeb8df6bbf82157025de576c25fa49e6d6f0d95
AguzzTN54 <gusagusnanang@gmail.com>
2023-05-14 23:13:29 +0700

    Static Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
