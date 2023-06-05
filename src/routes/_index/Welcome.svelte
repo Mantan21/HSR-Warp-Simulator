b762cdab8842200697f13153978c8ec17be1398c
AguzzTN54 <gusagusnanang@gmail.com>
2023-06-05 16:04:50 +0700

    Make The site Public

---
+++ src/routes/_index/Welcome.svelte
@@ -0,0 +1,277 @@
+<script>
+	import { getContext, onMount } from 'svelte';
+	import { t } from 'svelte-i18n';
+	import { fade, fly } from 'svelte/transition';
+	import { logs } from '$lib/data/logs.json';
+	import { proUser } from '$lib/stores/app-store';
+	import accessKey from '$lib/helpers/access-key';
+	import { randomTrack } from '$lib/helpers/sounds/phonograph';
+	import { browserDetect } from '$lib/helpers/mobile-detect';
+	import { playSfx } from '$lib/helpers/sounds/audiofx';
+
+	let adKeyValid = false;
+	let savedKey = '';
+	let dateExpired = '';
+
+	onMount(async () => {
+		const { validity, expiryDate, storedKey } = await accessKey.initialLoad();
+		adKeyValid = validity;
+		savedKey = storedKey;
+		dateExpired = expiryDate;
+	});
+
+	const closeWelcomeScreen = getContext('closeGreeting');
+	const handleSubmit = () => {
+		playSfx();
+		randomTrack('init');
+		proUser.set(adKeyValid);
+		closeWelcomeScreen();
+	};
+</script>
+
+<div class="welcome" out:fade>
+	<div class="container">
+		<div class="wrapper" out:fly={{ y: 25 }}>
+			<div class="header">
+				<h1>Honkai: Star Rail Warp Simulator</h1>
+
+				<h2>Welcome and Good Luck! 🙂</h2>
+
+				{#if !browserDetect().isSupported}
+					<div class="updates adExpired">
+						<strong>
+							We highly recommend you to install
+							<span style="display: inline; color:#5ab3ff"> Google Chrome </span>
+							First, Maybe some features won't work properly for now!
+						</strong>
+					</div>
+				{:else if dateExpired && dateExpired !== 'none'}
+					<div class="updates adExpired">
+						<div>
+							{@html $t('menu.keyExpiredNotice', {
+								values: { key: `<b>${savedKey}</b>`, date: `<b>${dateExpired}</b>` }
+							})}
+							<br />
+							<a
+								href="https://ko-fi.com/post/AdFree-Wish-Simulator-Enjoy-Simulator-Without-Ads-G2G2DQ57O"
+								target="_blank"
+							>
+								{$t('menu.getNewKey')}
+							</a>
+						</div>
+					</div>
+				{:else}
+					{@const { changes, date } = logs[logs.length - 1]}
+					<div class="updates">
+						<span>
+							<i class="tgl"> {date} </i>
+						</span>
+						{#each changes as txt} <p>{@html txt}</p> {/each}
+						<div style="height: .5rem" />
+					</div>
+				{/if}
+
+				<h3 class="disclaimer">
+					This Website is not affiliated with HoYoverse, all assets files are property of their
+					original owners
+				</h3>
+			</div>
+
+			<div class="footer">
+				<button on:click={handleSubmit}>
+					<i class="hsr-bulb" />
+					<span> Load Simulator </span>
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.welcome {
+		position: fixed;
+		z-index: 100;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: rgba(0, 0, 0, 0.2);
+		backdrop-filter: blur(10px);
+	}
+	a {
+		text-decoration: underline;
+		transition: all 0.25s;
+	}
+	a:hover {
+		color: orange;
+	}
+
+	.header {
+		padding: 3% 5%;
+	}
+
+	.container {
+		max-width: 90%;
+		width: 600px;
+		position: relative;
+	}
+	:global(.mobileLandscape) .container {
+		min-width: 400px;
+		width: 50%;
+	}
+
+	.wrapper {
+		color: #000;
+		background-color: rgba(255, 255, 255, 0.8);
+		text-align: center;
+		backdrop-filter: blur(10px);
+		border-top-right-radius: 5vh;
+		position: relative;
+		z-index: 2;
+	}
+
+	.container::before {
+		content: '';
+		position: absolute;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		height: 100%;
+		border: 2px solid var(--color-second);
+		mask-image: linear-gradient(45deg, black 50%, transparent 80%);
+		transform: translate(-1.5%, 3%);
+		opacity: 0.35;
+	}
+
+	h1 {
+		font-size: 200%;
+		color: rgb(207, 137, 7);
+		padding: 0 2% 3%;
+		border-bottom: 1px solid #ccc;
+	}
+	h2 {
+		font-size: 120%;
+		padding: 2%;
+	}
+
+	.footer {
+		background-color: rgba(0, 0, 0, 0.85);
+		padding: 3%;
+	}
+	button {
+		white-space: nowrap;
+		background-color: rgba(255, 255, 255, 0.85);
+		border-radius: 2rem;
+		padding: 2.5% 10%;
+		text-transform: uppercase;
+		display: inline-block;
+		line-height: 100%;
+		position: relative;
+		transition: all 0.2s;
+	}
+	button::after {
+		content: '';
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		height: 80%;
+		width: 96%;
+		border: 1px solid #bbb;
+		border-radius: 3rem;
+	}
+
+	button:not(:disabled):active {
+		opacity: 0.5;
+		transform: scale(0.95);
+	}
+
+	button i {
+		transform: translateX(-50%);
+		color: var(--color-second);
+		aspect-ratio: 1/1;
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+		font-size: 85%;
+	}
+	button i::after {
+		content: '';
+		width: 200%;
+		aspect-ratio: 1/1;
+		background-color: rgba(0, 0, 0, 0.85);
+		border: 1px solid var(--color-second);
+		border-radius: 100%;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		z-index: -1;
+	}
+
+	button span {
+		display: inline-block;
+		transform: translateX(6%);
+	}
+
+	/* Logs Container  */
+	.updates {
+		text-align: left;
+		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+		background-color: #fff;
+		font-size: 0.97rem;
+		height: calc(0.7 * var(--modalHeight));
+		padding: 0 1rem;
+		display: block;
+		overflow: hidden;
+	}
+	.updates span {
+		font-weight: bold;
+		color: #f7cf33;
+		display: block;
+		padding-top: 0.5rem;
+	}
+	.updates .tgl {
+		color: #bd6932;
+	}
+
+	.updates p {
+		padding-left: 1rem;
+		position: relative;
+		line-height: 1rem;
+		margin: 0.5rem 0;
+	}
+	.updates p::before {
+		content: '*';
+		display: inline-block;
+		width: 10px;
+		line-height: 0;
+		font-size: 1.3rem;
+		padding-top: 0.5rem;
+		position: absolute;
+		left: 0;
+		top: 50%;
+		transform: translateY(-50%);
+	}
+
+	.adExpired {
+		text-align: center;
+		font-size: 125%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 1rem;
+	}
+
+	.disclaimer {
+		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+		font-size: 90%;
+		font-weight: normal;
+		padding-top: 2%;
+		line-height: 100%;
+	}
+</style>
