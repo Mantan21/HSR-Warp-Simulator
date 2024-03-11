1b87c5ca803cae61f911cafc479d413088d0d89f
AguzzTN54 <gusagusnanang@gmail.com>
2024-03-11 14:34:05 +0700

    Fix Bug WHile changing current pity

--- src/routes/_warp/probability-editor.svelte
+++ src/routes/_warp/probability-editor.svelte
@@ -103,8 +103,8 @@
 
 		// Current Pity Changer
 		const value = parseInt(val);
-		if (variable === 'now4') localPity.set(`pity4-${banner}`, value);
-		if (variable === 'now5') localPity.set(`pity5-${banner}`, value);
+		if (variable === 'now4') localPity.set(`pity4${banner}`, value);
+		if (variable === 'now5') localPity.set(`pity5${banner}`, value);
 	};
 </script>
 
@@ -173,7 +173,7 @@
 					<div class="col">
 						<input
 							type="number"
-							value={localPity.get(`pity5-${banner}`)}
+							value={localPity.get(`pity5${banner}`)}
 							on:input={(e) => changePity(e, 'now5')}
 						/>
 					</div>
@@ -227,7 +227,7 @@
 					<div class="col">
 						<input
 							type="number"
-							value={localPity.get(`pity4-${banner}`)}
+							value={localPity.get(`pity4${banner}`)}
 							on:input={(e) => changePity(e, 'now4')}
 						/>
 					</div>
